3eec3a05e9cbfc087c7d70207589137b713134c5
Gaurish <gaurish108@gmail.com>
2022-03-06 02:24:42 -0500

    Revmoed pdfsforjoe from src directory

--- src/pdfsforjoe/script.sh
+++
@@ -1,9 +0,0 @@
-pdftk     1-segment-no-mesh.pdf     \
-1-segment-mesh.pdf        \
-1-segment-new-no-mesh.pdf \ 
-1-segment-new-mesh.pdf \
-2-segment-no-mesh.pdf  \
-2-segment-mesh.pdf     \
-5-segment-no-mesh.pdf  \
-5-segment-mesh.pdf     \
-cat output newfile.pdf
